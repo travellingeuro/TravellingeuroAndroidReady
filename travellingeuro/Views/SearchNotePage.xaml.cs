0228b4f19d88f464bfc0855aeed57e612ebe9134
Pedro Crespo <pedro.crespo.martin@gmail.com>
2020-11-02 12:12:12 -0600

    1101-added

--- travellingeuro/Views/SearchNotePage.xaml.cs
+++ travellingeuro/Views/SearchNotePage.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using MarcTron.Plugin;
+using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
@@ -17,6 +15,16 @@ namespace travellingeuro.Views
         public SearchNotePage()
         {
             InitializeComponent();
+            var videoid = Xamarin.Essentials.DeviceInfo.Platform == DevicePlatform.Android ? AppSettings.RewardVideoAndroid : AppSettings.RewardVideoiOS;
+            CrossMTAdmob.Current.LoadRewardedVideo(videoid);
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            if (CrossMTAdmob.Current.IsRewardedVideoLoaded())
+            {
+                CrossMTAdmob.Current.ShowRewardedVideo();
+            }
         }
     }
 }
